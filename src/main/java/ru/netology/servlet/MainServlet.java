5501fc17302cac32f42a57c5ad6bd835e7bf1855
ProkopenkoAV <arty1987@mail.ru>
2024-10-24 20:41:55 +0300

    complited di-java

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
@@ -13,9 +15,8 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        controller = context.getBean(PostController.class);
     }
 
     @Override
